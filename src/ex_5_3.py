18d87b0c67453b60c9f7f409cda7daf82aa069de
roshinishaik18 <149715528+roshinishaik18@users.noreply.github.com>
2023-12-12 16:10:45 -0600

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -6,6 +6,7 @@ This module contains an entry point that:
 - writes the file to the output file argument
 """
 import numpy as np
+import argparse
 from argparse import ArgumentParser
 
 if __name__ == "__main__":
@@ -16,4 +17,13 @@ if __name__ == "__main__":
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
-    pass
+    obj_parse = ArgumentParser(description="This program applies a standard scale transform to the data in infile and writes it to outfile")
+    obj_parse.add_argument("infile", type=argparse.FileType('r'))
+    obj_parse.add_argument("outfile", type=argparse.FileType('w'))
+    args_p = obj_parse.parse_args()
+    data = np.loadtxt("input_data.txt")
+    mean_val = np.mean(data)
+    mean_val1 = data - mean_val
+    std_data = np.std(mean_val1)
+    processed = mean_val1 / std_data
+    np.savetxt(obj_parse.outfile, processed,fmt='%.2e')
